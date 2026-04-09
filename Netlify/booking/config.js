3a543ea3cb39def84f1da3d199fd5920d3362dad
User <user@example.com>
2026-04-09 10:03:51 +0700

    Fix: Sync booking API URL with main app API URL

--- Netlify/booking/config.js
+++ Netlify/booking/config.js
@@ -4,4 +4,4 @@
  */
 
 // URL Google Apps Script Web App (Backend Utama)
-const LICENSE_API_URL = "https://script.google.com/macros/s/AKfycbwOp6S8pLu5lXN8NdKsaEvUpkq7JO6-83DO_H0A-EUSbVHRB2C2a95ZBFV4w4-T9lI/exec";
+const LICENSE_API_URL = "https://script.google.com/macros/s/AKfycbzNH8sl_clGt6F9lce9ySAHQJlG9v88rDR8WQWlwSOKZItHGGIttcimH8323mbd7rc8/exec";
